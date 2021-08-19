18fc86301e180ab686476cdad9efce6b89900f7f
Abhishek Agnihotri <agni.abhi00@gmail.com>
2021-08-19 18:14:51 +0530

    add intent details section

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -2,28 +2,71 @@ import React, { useEffect, useState } from "react";
 import axios, { AxiosRequestConfig } from 'axios';
 import './css/homePage.scss'
 
+
+
+
 function DataRow(data: any) {
   const intent = data.intent;
   console.log(intent);
-  function handleClick(e:any) {
-    data.history.push(`/intent?id=${intent.name}`, { intentData: e })
-  }
-
   return (
-    <div className='intent_row' onClick={() => handleClick(intent)}>
-      {/* <div className='intent_name'><p>{intent.name}</p></div> */}
+    <div className='intent_row' onClick={() => {
+      data.clickHandler(intent);
+      data.showMoreDetails(true);
+    }}>
       <div className='intent_display_name row'><p>{intent.displayName}</p></div>
-      {/* <div className='action row'><p>{intent.action}</p></div> */}
-      {/* <div className='folloup_name row'><p>{intent.rootFollowupIntentName}</p></div> */}
-      {/* <div className='parent_follow_up row'><p>{intent.parentFollowupIntentName}</p></div> */}
-      {/* <div className='context_display row'><p>{JSON.stringify(intent.outputContexts)}</p></div> */}
-      {/* <div className='inputContext row'><p>{JSON.stringify(intent.inputContextNames)}</p></div> */}
+    </div>
+  )
+}
+
+function ShowDetails(data: any) {
+  const intent = data.intentData;
+  return (
+    <div className='details'>
+      <div className='details_container'>
+        <h4>Display Name :- </h4>
+        <div className='intent_display_name row'><h4>{intent.displayName}</h4></div>
+        <h5>Intent name:- </h5>
+        <div className='intent_name row'><p>{intent.name}</p></div>
+        <h5>Action :- </h5>
+        <div className='action row'><p>{intent.action}</p></div>
+        <h5>Root Followup Intent Name :- </h5>
+        <div className='folloup_name row'><p>{intent.rootFollowupIntentName}</p></div>
+        <h5>Parent Followup Intent Name :- </h5>
+        <div className='parent_follow_up row'><p>{intent.parentFollowupIntentName}</p></div>
+        <h5>Output Contexts:- </h5>
+        <div className='context_display row'>
+          {
+            (intent.outputContexts) ? intent.outputContexts.map((context: any) => {
+              return (
+                <div className='context'>
+                  <p>{context.name}</p>
+                  <p>{JSON.stringify(context.parameters.fields)}</p>
+                </div>
+              )
+            })
+            : <p>None</p>}
+        </div>
+        <h5>Input context names :- </h5>
+        <div className='input_context row'>
+          {
+            (intent.inputContextNames) ?
+              intent.inputContextNames.map((inputContext:any) => {
+                return (
+                  <div>{inputContext}</div>
+                )
+              })
+              : <div>None</div>
+          }
+        </div>
+      </div>
     </div>
   )
 }
 
 export default function Home(props: { history: any; }) {
   const [intentData, setIntentData] = useState(Array);
+  const [displayIntentData, setDisplayIntent] = useState(Object);
+  const [showMoreDetails, setShowMoreDetails] = useState(false);
 
   useEffect(() => {
     var config: AxiosRequestConfig = {
@@ -37,19 +80,24 @@ export default function Home(props: { history: any; }) {
       .catch(function (error: any) {
         console.log(error);
       });
-  }, []);
+  }, [setIntentData]);
+
 
   return (
     <div className='main_container'>
-      <div className='data_container'>
-        <div className='row_container'>
-          {intentData.map((data, i) => {
-            return (
-              <DataRow key={i} intent={data} history={props.history}/>
-            )
-          })}
+      <div className='data'>
+        <h3>Intent List</h3>
+        <div className='data_container'>
+          <div className='row_container'>
+            {intentData.map((data, i) => {
+              return (
+                <DataRow key={i} intent={data} history={props.history} clickHandler={setDisplayIntent} showMoreDetails={setShowMoreDetails}/>
+              )
+            })}
+          </div>
         </div>
       </div>
+      { (showMoreDetails)? <ShowDetails intentData={displayIntentData} /> : <span className='hidden'></span> }
     </div>
   );
 }
